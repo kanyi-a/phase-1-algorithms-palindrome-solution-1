fe59548582fb6817d1aa442878a3f253739961b1
angela kanyi <angela.kanyi@student.moringaschool.com>
2022-06-18 21:16:49 +0300

    commit tree 43776673985e80e328b7ea2ca993ff9ad68e6af9

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
+
+function reverseString(word) {
+  return word.split("").reverse().join("");
+}
+
+
+
 function isPalindrome(word) {
   // Write your algorithm here
+  const reversedWord = reverseString(word);
+  if (word === reversedWord) {
+    return true;
+  } else {
+    return false;
+  }
 }
+
 
 /* 
   Add your pseudocode here
